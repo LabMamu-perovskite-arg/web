5e388c68cf0a8e55095207061aef945841c6f952
LabMamu-perovskite-arg <155489593+LabMamu-perovskite-arg@users.noreply.github.com>
2024-01-03 23:46:18 -0300

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,3 @@
-
 //Cambiar la pagina a Español
 function indexESP() {
     window.location.href = 'index.html';
@@ -21,3 +20,17 @@ function desactivarArrastre(event) {
     event.preventDefault();
 }
 
+
+// Slider
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sliderNavLinks = document.querySelectorAll(".slider-nav a");
+    const slider = document.querySelector(".slider");
+
+    sliderNavLinks.forEach((link, index) => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            slider.scrollLeft = index * slider.offsetWidth;
+        });
+    });
+});
